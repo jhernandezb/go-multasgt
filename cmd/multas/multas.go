d2cb02fe01ef1470a6a0ac97fab576f1081991ac
jhernandez <contact@jhernandez.me>
2016-12-11 23:08:12 -0600

    GetAllTickets

--- cmd/multas/multas.go
+++ cmd/multas/multas.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"sync"
 
 	"net/http"
 
@@ -19,86 +18,7 @@ func main() {
 	var pType = flag.String("type", "P", "Plate Type")
 	var pNumber = flag.String("number", "123ABC", "Plate Number")
 	flag.Parse()
-	var wg sync.WaitGroup
-	var mutex sync.Mutex
-	wg.Add(7)
-	var ts []multasgt.Ticket
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		e := &multasgt.Emetra{}
-		e.Client = client
-		tickets, _ = e.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		em := &multasgt.Emixtra{}
-		em.Client = client
-		tickets, _ = em.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		scp := &multasgt.SCP{}
-		scp.Client = client
-		tickets, _ = scp.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		f := &multasgt.Fraijanes{}
-		f.Client = client
-		tickets, _ = f.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		v := &multasgt.VillaNueva{}
-		v.Client = client
-		tickets, _ = v.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		p := &multasgt.PNC{}
-		p.Client = client
-		tickets, _ = p.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var tickets []multasgt.Ticket
-		a := &multasgt.Antigua{}
-		a.Client = client
-		tickets, _ = a.Check(*pType, *pNumber)
-		mutex.Lock()
-		ts = append(ts, tickets...)
-		mutex.Unlock()
-	}()
-	wg.Wait()
+	ts, _ := multasgt.GetAllTickets(*pType, *pNumber, client)
 	for _, t := range ts {
 		fmt.Printf("%#v \n", t)
 	}
---
+++ tickets.go
@@ -0,0 +1,48 @@
+package multasgt
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+// Tickets represents all collected tickets
+type Tickets struct {
+	Tickets []Ticket
+	M       sync.Mutex
+}
+
+func getTickets(wg *sync.WaitGroup,
+	plateType, plateNumber string,
+	tckts *Tickets,
+	checker TicketChecker) {
+	fmt.Println("Getting tickets")
+	defer wg.Done()
+	var tickets []Ticket
+	tickets, _ = checker.Check(plateType, plateNumber)
+	tckts.M.Lock()
+	tckts.Tickets = append(tckts.Tickets, tickets...)
+	tckts.M.Unlock()
+}
+
+func GetAllTickets(plateType, plateNumber string, c *http.Client) ([]Ticket, error) {
+	var wg sync.WaitGroup
+	wg.Add(7)
+	var tickets Tickets
+	// Emetra
+	go getTickets(&wg, plateType, plateNumber, &tickets, &Emetra{Client: c})
+	// Mixco
+	go getTickets(&wg, plateType, plateNumber, &tickets, &Emixtra{Client: c})
+	// SCP
+	go getTickets(&wg, plateType, plateNumber, &tickets, &SCP{Client: c})
+	// Fraijanes
+	go getTickets(&wg, plateType, plateNumber, &tickets, &Fraijanes{Client: c})
+	// Antigua
+	go getTickets(&wg, plateType, plateNumber, &tickets, &Antigua{Client: c})
+	// PNC
+	go getTickets(&wg, plateType, plateNumber, &tickets, &PNC{Client: c})
+	// VillaNueva
+	go getTickets(&wg, plateType, plateNumber, &tickets, &VillaNueva{Client: c})
+	wg.Wait()
+	return tickets.Tickets, nil
+}
